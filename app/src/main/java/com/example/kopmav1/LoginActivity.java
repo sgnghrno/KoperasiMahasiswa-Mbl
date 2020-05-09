075bae8f01649620209403a10c38a3b9e8a9ae7e
sgnghrno <sgnghrno@gmail.com>
2020-05-09 23:26:23 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/kopmav1/LoginActivity.java
+++ app/src/main/java/com/example/kopmav1/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.kopmav1;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenKeuanganActivity();
+            }
+        });
+    }
+
+    public void OpenKeuanganActivity(){
+        Intent intent = new Intent(this, KeuanganActivity.class);
+        startActivity(intent);
     }
 }
