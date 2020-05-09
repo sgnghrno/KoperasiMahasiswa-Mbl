d6e2d1610d11e229da9ea64eaf8cba0558ad29cf
sgnghrno <sgnghrno@gmail.com>
2020-05-09 23:26:29 +0700

    Create KeuanganActivity.java

---
+++ app/src/main/java/com/example/kopmav1/KeuanganActivity.java
@@ -0,0 +1,22 @@
+package com.example.kopmav1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class KeuanganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keuangan);
+        // nanti saat php image view ditambah  android:onClick="onKlik"
+
+    }
+}
