2b2982c5dacc24ceedf0aa9253b15e3a4b8619d4
sgnghrno <sgnghrno@gmail.com>
2020-06-21 20:23:10 +0700

    Create TamrikActivity.java

---
+++ app/src/main/java/com/example/kopmav1/TamrikActivity.java
@@ -0,0 +1,28 @@
+package com.example.kopmav1;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TamrikActivity extends AppCompatActivity {
+    private ProgressDialog progress;
+    Button simpan, keluar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tamrik);
+
+        keluar = (Button) findViewById(R.id.keluar);
+        keluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent hapus = new Intent(TamrikActivity.this,TarikActivity.class);
+                startActivity(hapus);
+            }
+        });
+    }
+}
