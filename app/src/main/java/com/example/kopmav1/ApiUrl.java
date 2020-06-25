1c5d9678f3fb6d1c323743aba841f538a0584270
sgnghrno <sgnghrno@gmail.com>
2020-06-25 21:57:10 +0700

    Update ApiUrl.java

--- app/src/main/java/com/example/kopmav1/ApiUrl.java
+++ app/src/main/java/com/example/kopmav1/ApiUrl.java
@@ -2,4 +2,5 @@ package com.example.kopmav1;
 
 public class ApiUrl {
     public static final String Api_Insert = "http://172.20.10.4/project-ci/Api/ApiInsert";
+    public static final String Api_Insert1 = "http://172.20.10.4/project-ci/Api/ApiInsert1";
 }
