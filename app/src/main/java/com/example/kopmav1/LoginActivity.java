078d714434b7b2a78a2f295a156a13a317c7d870
sgnghrno <sgnghrno@gmail.com>
2020-06-25 14:02:19 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/kopmav1/LoginActivity.java
+++ app/src/main/java/com/example/kopmav1/LoginActivity.java
@@ -1,6 +1,6 @@
 package com.example.kopmav1;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
