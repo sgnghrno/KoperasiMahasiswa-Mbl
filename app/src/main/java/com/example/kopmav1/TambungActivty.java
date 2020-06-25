f831d8ad2cc0b3e7bb6db816a58de207cd1a8703
sgnghrno <sgnghrno@gmail.com>
2020-06-25 14:03:21 +0700

    Update TambungActivty.java

--- app/src/main/java/com/example/kopmav1/TambungActivty.java
+++ app/src/main/java/com/example/kopmav1/TambungActivty.java
@@ -2,46 +2,162 @@ package com.example.kopmav1;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Toast;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.DatagramSocket;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TambungActivty extends AppCompatActivity {
-    private ProgressDialog progress;
+    private ProgressDialog pd;
     Button simpan, keluar;
+    EditText id_anggota, setoran;
+
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambung_activty);
 
+        queue = Volley.newRequestQueue(this);
+
+        /*get data from intent*/
+        Intent data = getIntent();
+        final int update = data.getIntExtra("update",0);
+        String intent_id_anggota = data.getStringExtra("id_anggota");
+        String intent_setoran = data.getStringExtra("setoran");
+        /*end get data from intent*/
+
+        id_anggota = (EditText) findViewById(R.id.id_anggota);
+        setoran = (EditText) findViewById(R.id.setoran);
         keluar = (Button) findViewById(R.id.keluar);
+        simpan = (Button) findViewById(R.id.simpan);
+        pd = new ProgressDialog(TambungActivty.this);
+
+        /*kondisi update / insert*/
+//        if(update == 1)
+//        {
+//            simpan.setText("Update Data");
+//            id_anggota.setText(intent_id_anggota);
+//            //id_anggota.setVisibility(View.GONE);
+//            setoran.setText(intent_setoran);
+//
+//        }
+
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                simpanData();
+            }
+        });
+
         keluar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent hapus = new Intent(TambungActivty.this,TabungActivity.class);
-                startActivity(hapus);
+                Intent main = new Intent(TambungActivty.this,TabungActivity.class);
+                startActivity(main);
             }
         });
+    }
+
+    private void Update_data()
+    {
+        pd.setMessage("Update Data");
+        pd.setCancelable(false);
+        pd.show();
+
+        StringRequest updateReq = new StringRequest(Request.Method.POST, ApiUrl.Api_Insert,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        pd.cancel();
+                        try {
+                            JSONObject res = new JSONObject(response);
+                            Toast.makeText(TambungActivty.this, "pesan : "+   res.getString("message") , Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        startActivity( new Intent(TambungActivty.this,TabungActivity.class));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pd.cancel();
+                        Toast.makeText(TambungActivty.this, "pesan : Gagal Insert Data", Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>();
+                map.put("id_anggota",id_anggota.getText().toString());
+                map.put("setoran",setoran.getText().toString());
+
+                return map;
+            }
+        };
+
+     //   AppController.getInstance().addToRequestQueue(updateReq);
+    }
+
+
+
+    private void simpanData()
+    {
+
+        pd.setMessage("Menyimpan Data");
+        pd.setCancelable(false);
+        pd.show();
+
+        StringRequest sendData = new StringRequest(Request.Method.POST, ApiUrl.Api_Insert,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        pd.cancel();
+                        try {
+                            JSONObject res = new JSONObject(response);
+                            Toast.makeText(TambungActivty.this, "pesan : "+   res.getString("message") , Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        startActivity( new Intent(TambungActivty.this,TabungActivity.class));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pd.cancel();
+                        Toast.makeText(TambungActivty.this, "pesan : Gagal Insert Data", Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>();
+                map.put("id_anggota",id_anggota.getText().toString());
+                map.put("setoran",setoran.getText().toString());
+                return map;
+            }
+        };
+
+        queue.add(sendData);
     }
 }
