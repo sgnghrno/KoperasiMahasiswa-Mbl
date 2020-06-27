6b49d8d9a63523d02bbee94db51921e804135b6d
sgnghrno <sgnghrno@gmail.com>
2020-06-27 20:03:49 +0700

    Update ApiUrl.java

--- app/src/main/java/com/example/kopmav1/ApiUrl.java
+++ app/src/main/java/com/example/kopmav1/ApiUrl.java
@@ -1,7 +1,13 @@
 package com.example.kopmav1;
 
 public class ApiUrl {
-    public static final String Api_Data = "http://172.20.10.4/project-ci/Api";
-    public static final String Api_Insert = "http://172.20.10.4/project-ci/Api/ApiInsert";
-    public static final String Api_Insert1 = "http://172.20.10.4/project-ci/Api/ApiInsert1";
+    public static final String Api_Insert = "http://sikopma.mif-project.com/Api/ApiInsert";
+    public static final String Api_Insert1 = "http://sikopma.mif-project.com/Api/ApiInsert1";
+
+//    url, IP disesuaikan dengan local ip, atau domain internet
+    public static final String LOGIN = "http://sikopma.mif-project.com/Api/login";
+    public static final String HOME = "http://sikopma.mif-project.com/Api";
+    public static final String TABUNGAN = "http://sikopma.mif-project.com/Api/tabungan";
+    public static final String TARIKAN = "http://sikopma.mif-project.com/Api/tarikan";
+    public static final String ANGGOTA = "http://sikopma.mif-project.com/Api/anggota";
 }
