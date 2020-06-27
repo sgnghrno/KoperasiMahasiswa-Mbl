3010935f7a3ef00b9be3e5eea5c50781ad02e463
sgnghrno <sgnghrno@gmail.com>
2020-06-27 20:07:00 +0700

    Update ModelData.java

--- app/src/main/java/com/example/kopmav1/ModelData.java
+++ app/src/main/java/com/example/kopmav1/ModelData.java
@@ -1,17 +1,19 @@
 package com.example.kopmav1;
 
 public class ModelData {
-    String id_anggota, setoran;
-
-    public ModelData(){}
+    private String id_anggota, setoran;
 
     public ModelData(String id_anggota, String setoran){
-        this.id_anggota = id_anggota;
-        this.setoran = setoran;
+        id_anggota = id_anggota;
+        setoran = setoran;
     }
 
+    public void setId_anggota(String id_anggota) {
+        this.id_anggota = id_anggota;
+    }
     public String  getId_anggota() {return id_anggota;}
-    public void setId_anggota (String id_anggota) {this.id_anggota = id_anggota;}
+    public void setSetoran(String setoran) {
+        this.setoran = setoran;
+    }
     public String getSetoran() {return setoran;}
-    public void setSetoran() {this.setoran=setoran;}
 }
