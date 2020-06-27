5e9a3cc2008fa0cc07afd35ad6396fe1e23a7fad
sgnghrno <sgnghrno@gmail.com>
2020-06-27 20:06:55 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/kopmav1/MainActivity.java
+++ app/src/main/java/com/example/kopmav1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.kopmav1;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kopmav1.home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,22 +15,33 @@ public class MainActivity extends AppCompatActivity {
     Runnable runnable;
     ImageView img;
 
+//    untuk cek apakah sudah login
+    SessionManager sessionManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img = findViewById(R.id.img);
-        img.animate().alpha(4000).setDuration(0);
+        sessionManager = new SessionManager(this);
 
-        handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent dsp = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(dsp);
-                finish();
-            }
-        },4000);
+        if (sessionManager.isLogin()){
+            Intent keHome = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(keHome);
+            finish();
+        } else {
+            img = findViewById(R.id.img);
+            img.animate().alpha(4000).setDuration(0);
+
+            handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent dsp = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(dsp);
+                    finish();
+                }
+            },4000);
+        }
     }
 }
